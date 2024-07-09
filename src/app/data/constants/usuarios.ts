fd5c03959b4f9b67de53167a34f5ac2b4423492c
João Baraldi <jmbbaraldi@gmail.com>
2024-07-09 07:04:17 -0300

    First commit

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,42 @@
+import { Usuario } from '@/core/model/Usuario';
+
+const usuarios: Usuario[] = [
+    {
+        id: "1",
+        email: "joao@empresa.com.br",
+        nome: "Joao",
+        senha: "123456"
+    },
+    {
+        id: "2",
+        email: "adrialdo@empresa.com.br",
+        nome: "Adrialdo",
+        senha: "123456"
+    },
+    {
+        id: "3",
+        email: "sargento@empresa.com.br",
+        nome: "Sargento",
+        senha: "123456"
+    },
+    {
+        id: "4",
+        email: "cadu@empresa.com.br",
+        nome: "Cadu",
+        senha: "123456"
+    },
+    {
+        id: "5",
+        email: "realcaleb@empresa.com.br",
+        nome: "Caleb",
+        senha: "123456"
+    },
+    {
+        id: "6",
+        email: "enzolora@empresa.com.br",
+        nome: "Enzo",
+        senha: "123456"
+    }
+]
+
+export default usuarios
